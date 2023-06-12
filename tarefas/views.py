e4e5502c95572741bfefe15cff0f9f00c250304d
Diego Neves <pdcassiano@gmail.com>
2023-06-12 17:16:48 -0300

    form, crispy form

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarefa
+from .forms import AdicionarTarefa
 
 # Create your views here.
 def tarefas_pendentes_list(request):
     tarefas_pendentes = Tarefa.objects.filter(status='pendente')
-    return render(request, 'tarefas/tarefas_pendentes.html', {'tarefas_pendentes': tarefas_pendentes})
+    if request.method == 'POST':
+        form = AdicionarTarefa(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tarefas_pendentes_list') 
+    else:
+        form = AdicionarTarefa()
+        
+    return render(request, 'tarefas/tarefas_pendentes.html', {'tarefas_pendentes': tarefas_pendentes, 'form': form})
