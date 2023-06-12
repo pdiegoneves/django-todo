a4c11abde0b78ecbb9c9d57ea30c2530ae4a5cf9
Diego Neves <pdcassiano@gmail.com>
2023-06-12 13:36:24 -0300

    criado a model tarefas

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+    OPCOES_STATUS = (
+        ('concluído', 'Concluído'),
+        ('pendente', 'Pendente'),
+        ('adiado', 'Adiado'),
+    )
+
+    OPCOES_CATEGORIAS = (
+        ('urgente', 'Urgente'),
+        ('importante', 'Importante'),
+        ('precisa ser feito', 'Precisa ser feito'),
+    )
+
+    descricao = models.CharField(max_length=400)
+    criacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.CharField(max_length=25, choices=OPCOES_CATEGORIAS, default='importante')
+    status = models.CharField(max_length=25, choices=OPCOES_STATUS, default='pendente')
+
+    
